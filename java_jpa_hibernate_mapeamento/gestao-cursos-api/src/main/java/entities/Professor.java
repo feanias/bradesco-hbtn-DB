e298e7448d4e3b4624fb9f3759655314ca615cfc
Felipe Anias <felipeanias@hotmail.com>
2025-10-15 22:26:20 -0300

    exercicio 1

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Professor.java
@@ -1,8 +1,9 @@
+
 package entities;
 
-import jakarta.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "professor")
@@ -12,24 +13,104 @@ public class Professor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
-    private String nome;
+    @Column(name = "nome_completo", nullable = false)
+    private String nomeCompleto;
 
-    @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL, orphanRemoval = true)
-    private Set<Curso> cursos = new HashSet<>();
+    @Column(name = "matricula", nullable = false, unique = true)
+    private String matricula;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Telefone> telefones = new ArrayList<>();
+
+    @OneToMany(mappedBy = "professor", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Curso> cursos = new ArrayList<>();
 
     public Professor() {}
-    public Professor(String nome) { this.nome = nome; }
 
-    // getters/setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    public String getNome() { return nome; }
-    public void setNome(String nome) { this.nome = nome; }
-    public Set<Curso> getCursos() { return cursos; }
+    public Professor(String nomeCompleto, String matricula, String email) {
+        this.nomeCompleto = nomeCompleto;
+        this.matricula = matricula;
+        this.email = email;
+    }
 
-    public void addCurso(Curso c) {
-        cursos.add(c);
-        c.setProfessor(this);
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Telefone> getTelefones() {
+        return telefones;
+    }
+
+    public void setTelefones(List<Telefone> telefones) {
+        this.telefones = telefones;
+    }
+
+    public List<Curso> getCursos() {
+        return cursos;
+    }
+
+    public void setCursos(List<Curso> cursos) {
+        this.cursos = cursos;
+    }
+
+    public void addTelefone(Telefone telefone) {
+        telefones.add(telefone);
+        telefone.setProfessor(this);
+    }
+
+    public void removeTelefone(Telefone telefone) {
+        telefones.remove(telefone);
+        telefone.setProfessor(null);
+    }
+
+    public void addCurso(Curso curso) {
+        cursos.add(curso);
+        curso.setProfessor(this);
+    }
+
+    public void removeCurso(Curso curso) {
+        cursos.remove(curso);
+        curso.setProfessor(null);
+    }
+
+    @Override
+    public String toString() {
+        return "Professor{" +
+                "id=" + id +
+                ", nomeCompleto='" + nomeCompleto + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
